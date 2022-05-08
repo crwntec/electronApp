41c6b91b4301bdead7c645e3112f4deea64f7b05
Neuron Electronics <develop.neuron@gmail.com>
2022-05-08 10:57:30 +0200

    Initial Commit & import from old project

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+const createWindow = (w, h) => {
+  const win = new BrowserWindow({
+    width: w,
+    height: h,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  win.loadFile(path.join(__dirname, 'src/index.html'));
+  win.removeMenu();
+};
+
+app.whenReady().then(() => {
+  createWindow(800, 600);
+  ipcMain.on('synchronous-message', (e) => {
+    e.returnValue = process.platform;
+  });
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow(800, 600);
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.exit(0);
+});
